1c17d1bb0a214ce1d99ee245b524e23abe5d275a
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-06-20 20:15:57 +0300

    Added video reading with FPS, frame size, frame count and ability to speed up the video (changing the time for frame in writeKey method)

--- assets/assets.py
+++ assets/assets.py
@@ -1,1 +1,2 @@
 cat_image = '/Users/southrussian/PycharmProjects/read-write-images/assets/cat.jpeg'
+cat_video = '/Users/southrussian/PycharmProjects/read-write-images/assets/PianoCat.mp4'
--- main.py
+++ main.py
@@ -3,22 +3,63 @@ from assets import assets
 import sys
 
 
-def main():
-    file = assets.cat_image
-    image = cv.imread(file, cv.IMREAD_GRAYSCALE)
+# def main():
+#     file = assets.cat_image
+#     image = cv.imread(file, cv.IMREAD_GRAYSCALE)
+#
+#     if image is None:
+#         sys.exit('Could not read the image')
+#
+#     cv.imshow('Cat Image', image)
+#     key = cv.waitKey(0)
+#
+#     if key == ord('s'):
+#         cv.imwrite('outputCat.png', image)
+#
+#     cv.destroyAllWindows()
+#     sys.exit('Finished!')
 
-    if image is None:
-        sys.exit('Could not read the image')
+# if __name__ == '__main__':
+#     main()
 
-    cv.imshow('Cat Image', image)
-    key = cv.waitKey(0)
 
-    if key == ord('s'):
-        cv.imwrite('outputCat.png', image)
+def play_video(file: str, webcam: bool = False):
+    if webcam:
+        video = cv.VideoCapture(0)
+    else:
+        video = cv.VideoCapture(file)
 
-    cv.destroyAllWindows()
-    sys.exit('Finished!')
+    if not video.isOpened():
+        print('Error opening the video file')
+    else:
+        fps = video.get(cv.CAP_PROP_FPS)
+        frame_count = video.get(cv.CAP_PROP_FRAME_COUNT)
+        width, height = int(video.get(cv.CAP_PROP_FRAME_WIDTH)), int(video.get(cv.CAP_PROP_FRAME_HEIGHT))
+        frame_size = (width, height)
+
+        print('Video details:')
+        print('FPS:', round(fps, 2))
+        print('Frame count:', frame_count)
+        print(f'H {height}, W {width}')
+
+        codec = cv.VideoWriter_fourcc(*'mp4v')
+        output = cv.VideoWriter('output.mp4', codec, fps, frame_size)
+
+        while video.isOpened():
+            success, frame = video.read()
+
+            if success:
+                cv.imshow('Frame', frame)
+                output.write(frame)
+                cv.waitKey(30)
+            else:
+                print('Finished')
+                break
+
+        video.release()
+        cv.destroyAllWindows
 
 
 if __name__ == '__main__':
-    main()
+    play_video(file=assets.cat_video)
+
