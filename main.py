1cbf056cf703ea574ebd35c5714a010b60927d70
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-06-21 16:31:57 +0300

    Resizing image

--- main.py
+++ main.py
@@ -22,44 +22,69 @@ import sys
 # if __name__ == '__main__':
 #     main()
 
+# video
 
-def play_video(file: str, webcam: bool = False):
-    if webcam:
-        video = cv.VideoCapture(0)
-    else:
-        video = cv.VideoCapture(file)
+# def play_video(file: str, webcam: bool = False):
+#     if webcam:
+#         video = cv.VideoCapture(0)
+#     else:
+#         video = cv.VideoCapture(file)
+#
+#     if not video.isOpened():
+#         print('Error opening the video file')
+#     else:
+#         fps = video.get(cv.CAP_PROP_FPS)
+#         frame_count = video.get(cv.CAP_PROP_FRAME_COUNT)
+#         width, height = int(video.get(cv.CAP_PROP_FRAME_WIDTH)), int(video.get(cv.CAP_PROP_FRAME_HEIGHT))
+#         frame_size = (width, height)
+#
+#         print('Video details:')
+#         print('FPS:', round(fps, 2))
+#         print('Frame count:', frame_count)
+#         print(f'H {height}, W {width}')
+#
+#         codec = cv.VideoWriter_fourcc(*'mp4v')
+#         output = cv.VideoWriter('output.mp4', codec, fps, frame_size)
+#
+#         while video.isOpened():
+#             success, frame = video.read()
+#
+#             if success:
+#                 cv.imshow('Frame', frame)
+#                 output.write(frame)
+#                 cv.waitKey(10)
+#             else:
+#                 print('Finished')
+#                 break
+#
+#         video.release()
+#         cv.destroyAllWindows
+#
+#
+# if __name__ == '__main__':
+#     play_video(file=assets.cat_video)
 
-    if not video.isOpened():
-        print('Error opening the video file')
-    else:
-        fps = video.get(cv.CAP_PROP_FPS)
-        frame_count = video.get(cv.CAP_PROP_FRAME_COUNT)
-        width, height = int(video.get(cv.CAP_PROP_FRAME_WIDTH)), int(video.get(cv.CAP_PROP_FRAME_HEIGHT))
-        frame_size = (width, height)
+def resize_image(percent: float):
+    file = assets.cat_image
+    image = cv.imread(file)
 
-        print('Video details:')
-        print('FPS:', round(fps, 2))
-        print('Frame count:', frame_count)
-        print(f'H {height}, W {width}')
+    image_dimension = image.shape
+    print('Dimensions:', image_dimension)
+    height, width = image_dimension[:2]
 
-        codec = cv.VideoWriter_fourcc(*'mp4v')
-        output = cv.VideoWriter('output.mp4', codec, fps, frame_size)
+    new_height = int(height * percent)
+    new_width = int(width * percent)
+    new_dimension = (new_height, new_width)
 
-        while video.isOpened():
-            success, frame = video.read()
+    resized = cv.resize(image, new_dimension, interpolation=cv.INTER_LINEAR)
 
-            if success:
-                cv.imshow('Frame', frame)
-                output.write(frame)
-                cv.waitKey(10)
-            else:
-                print('Finished')
-                break
+    print('New dimension:', resized.shape)
+    cv.imshow('Resized image', resized)
+    cv.waitKey(0)
 
-        video.release()
-        cv.destroyAllWindows
+    cv.destroyAllWindows()
+    sys.exit('Finished')
 
 
 if __name__ == '__main__':
-    play_video(file=assets.cat_video)
-
+    resize_image(0.1)
